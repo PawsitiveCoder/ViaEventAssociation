97749d87576546c9cf2412c8e5bacda76c34a3b7
Alex Tofan <7130188+savaryna@users.noreply.github.com>
2026-06-05 22:42:09 +0200

    core: add dependency injection for application command handlers

---
+++ Tests/UnitTests/Common/Services/DependencyInjectionUnitTests.cs
@@ -0,0 +1,24 @@
+using JetBrains.Annotations;
+using Microsoft.Extensions.DependencyInjection;
+using ViaEventAssociation.Core.AppEntry;
+using ViaEventAssociation.Core.Application;
+
+
+namespace UnitTests.Common.Services;
+
+[TestSubject(typeof(DependencyInjection))]
+public class DependencyInjectionUnitTests
+{
+  [Theory]
+  [InlineData(typeof(ICommandHandler<CreateEventCommand>))]
+  [InlineData(typeof(ICommandHandler<UpdateEventTitleCommand>))]
+  public void AddApplicationCommandHandlers_ShouldRegisterCommandHandlers(Type serviceType)
+  {
+    var services = new ServiceCollection();
+
+    services.AddApplicationCommandHandlers();
+
+    var serviceDescriptor = services.FirstOrDefault(sd => sd.ServiceType == serviceType);
+    Assert.NotNull(serviceDescriptor);
+  }
+}
